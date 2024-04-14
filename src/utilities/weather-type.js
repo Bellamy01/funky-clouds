70f6e22cad8e6b7e0f82ca9426b7a3873de6b422
Bellamy01 <bdanbellamy@gmail.com>
2024-04-14 15:27:16 +0200

    Fine tuning messages

--- src/utilities/weather-type.js
+++ src/utilities/weather-type.js
@@ -1,42 +1,48 @@
 export const weatherType = {
   Thunderstorm: {
     icon: 'zap',
-    message: 'It could get noisy!',
-    backgroundColor: '#000000'
+    message:
+      'Stay indoors! If you must be outside, avoid open areas, tall trees, and bodies of water.',
+    backgroundColor: '#000000',
   },
   Drizzle: {
     icon: 'cloud-rain',
-    message: 'It might rain a little',
-    backgroundColor: '#36454f'
+    message:
+      `Don't let a little drizzle dampen your spirits! Pack a light umbrella and enjoy the fresh air.`,
+    backgroundColor: '#36454f',
   },
   Rain: {
     icon: 'umbrella',
-    message: 'You will need an umbrella',
-    backgroundColor: '#0000ff'
+    message:
+      `It's raining cats and dogs! Grab your umbrella, or enjoy a cozy day indoors or put on your raincoat and splash in some puddles!`,
+    backgroundColor: '#0000ff',
   },
   Snow: {
     icon: 'cloud-snow',
-    message: 'Lets build a snowman!',
-    backgroundColor: '#7f6065'
+    message:
+      `Winter wonderland! Time to build a snowman, have a snowball fight, or just enjoy the beauty of a fresh snowfall. Remember to drive carefully!`,
+    backgroundColor: '#7f6065',
   },
   Clear: {
     icon: 'sun',
-    message: 'It is perfect t-shirt weather',
-    backgroundColor: '#e47200'
+    message: `Beautiful sunny day! Perfect for spending time outdoors, soaking up the vitamin D.`,
+    backgroundColor: '#e47200',
   },
   Clouds: {
     icon: 'cloud',
-    message: 'You could live in the clouds',
-    backgroundColor: '#363636'
+    message:
+      'A bit cloudy today. Still a good time to be outside, but be prepared for possible showers.',
+    backgroundColor: '#363636',
   },
   Haze: {
     icon: 'wind',
-    message: 'It might be a bit damp',
-    backgroundColor: '#58586e'
+    message: `It's a hazy day. Visibility might be reduced, so be extra cautious while driving.`,
+    backgroundColor: '#58586e',
   },
   Mist: {
     icon: 'align-justify',
-    message: 'It might be hard to see',
-    backgroundColor: '#3e3e37'
-  }
-}
+    message:
+      ' Misty morning! Take it slow on the roads and enjoy the mysterious atmosphere.',
+    backgroundColor: '#3e3e37',
+  },
+};
